65126fa4c7c87b053763b9cf2dc0094a75c8f226
Muhammad Sowrov <muhammadsowrov53@gmail.com>
2024-03-21 14:38:22 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import Router from './Router/Router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Router/Router";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <div>
+      <RouterProvider router={Router}></RouterProvider>
+    </div>
+  </React.StrictMode>
+);
