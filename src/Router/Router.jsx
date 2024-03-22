e5e65d38ed2d59fb13d45fbf7b15a185170e0fb8
Muhammad Sowrov <muhammadsowrov53@gmail.com>
2024-03-22 12:23:27 +0600

    modify route

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../Layout/RootLayout";
 import Home from "../Home/Home/Home";
+import Donate from "../Components/Donate/Donate";
+import About from "../Home/About/About";
+import Contact from "../Home/Contact/Contact";
 
 
 const Router = createBrowserRouter([
@@ -13,7 +16,16 @@ const Router = createBrowserRouter([
                 element: <Home></Home>
             },
             {
-                
+                path: "about",
+                element: <About></About>
+            },
+            {
+               path: "donate",
+               element: <Donate></Donate> 
+            },
+            {
+                path: "contact",
+                element: <Contact></Contact>
             }
         ]
     }
